dddd82be650980a5c1a03d03f27c1991652acd64
chadgevans <echad@sas.upenn.edu>
2017-11-10 08:35:13 -0500

    propensity score edits

--- lib/fn_bal.R
+++ lib/fn_bal.R
@@ -1,8 +1,8 @@
 fn_bal <- function(dta, variable) {
   dta$variable <- dta[, variable]
-  dta$TRAD_MAGNET <- as.factor(dta$TRAD_MAGNET)
+  dta$TREATMENT <- as.factor(dta$TREATMENT)
   support <- c(min(dta$variable), max(dta$variable))
-  ggplot(dta, aes(x = distance, y = variable, color = TRAD_MAGNET)) +
+  ggplot(dta, aes(x = distance, y = variable, color = TREATMENT)) +
     geom_point(alpha = 0.2, size = 1.3) +
     geom_smooth(method = "loess", se = F) +
     xlab("Propensity score") +
